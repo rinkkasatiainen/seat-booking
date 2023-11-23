3f652ec54e3932e9e7a49b266a69a0699806bf81
Aki Salmi <aki.salmi@iki.fi>
2023-11-23 16:23:24 +0200

    🔧: add wallaby

--- api/src/infra/postgres-db.ts
+++ api/src/infra/postgres-db.ts
@@ -7,7 +7,7 @@ function noop() {
 }
 
 type ConnectedPoolClient = Pick<PoolClient, 'query' | 'release'>
-type CanConnectToPool =Promise<ConnectedPoolClient>;
+type CanConnectToPool = Promise<ConnectedPoolClient>;
 
 export type CreatePool = () => CanConnectToPool
 export type DisconnectFromPool = () => Promise<void>
@@ -18,20 +18,6 @@ export interface ActsAsPool {
     disconnect: DisconnectFromPool;
 }
 
-function hasQ<T extends string>(key: T, data: FakeData): data is Fakeable<T> {
-    return Object.keys(data).includes(key)
-}
-function isDefined<T>(value: T | undefined ): asserts value is T {
-    if (value === undefined) {
-        // return false
-        throw new Error( 'Expected value not to be undefined' )
-    }
-}
-
-function assertValue<T>(x: T | undefined): T {
-    isDefined(x)
-    return x
-}
 
 export class PgPool {
 
@@ -41,6 +27,7 @@ export class PgPool {
     public connect() {
         return this.pool.connect()
     }
+
     public disconnect() {
         return this.pool.end()
     }
@@ -58,25 +45,35 @@ export class PgPool {
     }
 
     public static createNull(dataRows: FakeData = {}): ActsAsPool {
+        function isDefined<T>(value: T | undefined): asserts value is T {
+            if (value === undefined) {
+                throw new Error('Expected value not to be undefined')
+            }
+        }
+
+        function assertValue<T>(x: T | undefined): T {
+            isDefined(x)
+            return x
+        }
+
+        function dataOr<T extends string>(key: T, data: FakeData): unknown[] {
+            const value = data[key]
+            if (value === undefined) {
+                return []
+            }
+            return assertValue(value)
+        }
+        const resultOf: (query: string, data: unknown[]) => QueryResult = (query, rows) => ({
+            command: query, fields: [], oid: 0, rowCount: 0, rows,
+        })
+
         return {
             disconnect(): Promise<void> {
                 return Promise.resolve(undefined)
             },
             connect(): CanConnectToPool {
-                const empty: QueryResult = {
-                    command: 'none', fields: [], oid: 0, rowCount: 0, rows: [],
-                }
-                const resultOf: (query: string, data: unknown[]) => QueryResult = (query, rows) => ({
-                    command: query, fields: [], oid: 0, rowCount: 0, rows,
-
-                })
                 return Promise.resolve({
-                    query: (q: string) => {
-                        if (hasQ(q, dataRows)) {
-                            return Promise.resolve(resultOf(q, assertValue(dataRows[q])))
-                        }
-                        return Promise.resolve(empty)
-                    },
+                    query: (q: string) => Promise.resolve(resultOf(q, dataOr(q, dataRows))),
                     release: noop,
                 }) as Promise<ConnectedPoolClient>
             },
@@ -84,15 +81,8 @@ export class PgPool {
         }
     }
 }
-type FakeData = Record<string, unknown[]>
-type Fakeable<T extends string> = Record<T, unknown[]>
 
-const fff: FakeData = { test: ['foobar']}
-if( hasQ('test', fff) ){
-    const y: Fakeable<'test'> = fff
-    console.log(y)
-}
-console.log(fff)
+type FakeData = Record<string, unknown[]>
 
 const createPool: (x: Partial<PG_ENV>) => ActsAsPool = pgEnv => PgPool.of.call(this, pgEnv)
 
