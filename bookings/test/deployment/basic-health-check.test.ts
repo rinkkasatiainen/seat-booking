0012b364df4d40d0cea5963191b029e6f2d902aa
Aki Salmi <aki.salmi@iki.fi>
2023-12-15 12:23:27 +0200

    🔧: fix example docker dev-env for bookings

--- bookings/test/deployment/basic-health-check.test.ts
+++ bookings/test/deployment/basic-health-check.test.ts
@@ -1,7 +1,20 @@
+import chai from 'chai'
+import chaiSubset from 'chai-subset'
+import request from 'supertest'
 
 
-describe('Health of the Bookings component', () => {
-    it('Does listen to rabbitMQ', () => {
-        /* todo */
+const {expect} = chai
+chai.use(chaiSubset)
+
+describe('deployment', () => {
+    const rq = request('http://localhost:4100')
+
+    it('Should start server', async () => {
+        await rq.get('/health/check')
+            .expect(200)
+            .expect((res => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                expect(res.body.status).to.eql('ok')
+            }))
     })
 })
